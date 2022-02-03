33f327917a1767fde3d516ce3b5df74ffa72e547
Fernanda Herculano <fernandaherculanom@gmail.com>
2022-02-03 15:44:35 -0300

    Escolhendo arquivo

--- src/cliente.py
+++ src/cliente.py
@@ -3,14 +3,53 @@ from io import FileIO
 import socket 
 import threading
 
+def escolha_arquivo_cliente(cliente_socket):
+
+    # EXIBINDO AS OPÇÕES DE DOWNLOAD
+    print()
+    try:
+        opcoes = str(cliente_socket.recv(4096).rstrip().decode())
+        no_opcoes = len(opcoes.split("\n")) - 1
+        print(opcoes)
+    except Exception as e:
+        print("\nErro em obter opções:", e)
+        return
+
+    # ENVIANDO A OPÇÃO ESCOLHIDA
+    while True:
+        escolha = int(input("\nDigite o ID do arquivo a ser baixado: "))
+        if escolha <= no_opcoes and escolha > 0:
+            try:
+                cliente_socket.send(str(escolha).encode())
+                return 
+
+            except Exception as e:
+                print("\nErro em obter opções:", e)
+                continuar = input("\nDeseja digitar novamente? s/n: ")
+                if continuar == "n":
+                    return
+
+        else:
+            print("\nNúmero inválido.")
+            continuar = input("Deseja digitar novamente? s/n: ")
+            if continuar == "n":
+                return
+
+
+
+
+
 
 endereco = ("localhost", 12345)
 
 cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente_socket.connect(endereco)
 
+escolha_arquivo_cliente(cliente_socket)
+
+
 while True:
-    mensagem = input("Informe texto ou digite 'sair' para desconectar: ")
+    mensagem = input("\nInforme texto ou digite 'sair' para desconectar: ")
 
     if mensagem == "sair":
         cliente_socket.send(mensagem.encode())
--- src/servidor.py
+++ src/servidor.py
@@ -1,19 +1,54 @@
 import io
 from io import FileIO
 import socket 
+import os
 import threading 
   
+
+def escolha_arquivo(conexao):
+
+    # ENVIANDO OPÇÕES DE DOWNLOAD
+    arquivos = os.listdir(os.path.join(os.getcwd(), "../dados"))
+    opcoes = "Opções de download:\n"
+    i = 1
+    for item in arquivos:
+        opcoes += str(i) + ". " + item + "\n"
+        i += 1 
+
+    try:
+        conexao.sendall(str.encode(opcoes))
+    except Exception as e:
+        print("Erro no envio:", e)
+
+
+    # OBTENDO RESPOSTA
+    try:
+        resposta_opcao = int(conexao.recv(1024).rstrip().decode())
+        print("\nArquivo escolhido:",arquivos[resposta_opcao-1])
+    except Exception as e:
+        print("Erro na obtenção da opção:", e)
+
+
+
+
+
 endereco = ("localhost", 12345)
 servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 servidor_socket.bind(endereco)
 servidor_socket.listen(1)
 
-envio_servidor, endereco = servidor_socket.accept()
-print("Nova conexao recebida de ", endereco)
-
 while True:
     print("\nEscutando...")
+
+    # NOVA CONEXAO
+    envio_servidor, endereco = servidor_socket.accept()
+    print("\nNova conexao recebida de ", endereco)
+
+    # ENVIANDO OPCOES DE DOWNLOAD
+    escolha_arquivo(envio_servidor)
+
+    # RECEBENDO RESPOSTA DO CLIENTE
     resposta = envio_servidor.recv(1024)
     resposta = resposta.rstrip().decode()
 
