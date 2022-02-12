91e2fdc112e21fa9528e3bf91a37d5bfc0125265
Fernanda Herculano <fernandaherculanom@gmail.com>
2022-02-12 15:41:52 -0300

    Organizando funções

--- src/cliente.py
+++ src/cliente.py
@@ -1,7 +1,8 @@
-import io
-from io import FileIO
-import socket 
-import threading
+import socket
+import getopt
+import sys
+import os
+
 
 def escolha_arquivo_cliente(cliente_socket):
 
@@ -36,27 +37,133 @@ def escolha_arquivo_cliente(cliente_socket):
                 return
 
 
+def listar_arquivos(conexao):
+
+    print()
+    try:
+        lista_arquivos = str(conexao.recv(4096).rstrip().decode())
+        print(lista_arquivos)
+        input("Pressione enter...")
+
+    except Exception as e:
+        print("\nErro em obter opções:", e)
 
 
+def enviar_arquivos(conexao):
+    pass
 
 
-endereco = ("localhost", 12345)
-
-cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente_socket.connect(endereco)
-
-escolha_arquivo_cliente(cliente_socket)
-
-
-while True:
-    mensagem = input("\nInforme texto ou digite 'sair' para desconectar: ")
-
-    if mensagem == "sair":
-        cliente_socket.send(mensagem.encode())
-        cliente_socket.close()
-        break
+def baixar_arquivos(conexao):
     
-    else:
-        arquivo = open("../dados/"+mensagem, encoding="UTF-8")
-        cliente_socket.send(arquivo.read().encode())
-        arquivo.close()
+    # COMANDO
+    print()
+    id_escolhido = input("Digite o ID do arquivo escolhido (disponiveis na listagem): ")
+    
+    # ENVIANDO OPCAO
+    try:
+        conexao.send(id_escolhido.encode())
+
+    except Exception as e:
+        print("\nErro no envio:", e)
+        return
+
+    # CONFIRMACAO ARQUIVO
+    try:
+        confirmacao = conexao.recv(4096).rstrip().decode()
+        nome_arquivo = confirmacao.split(":")[-1]
+        tamanho_arquivo = int(confirmacao.split(":")[0])
+        
+        print(nome_arquivo)
+
+    except Exception as e:
+        print("\nErro na confirmacao:", e)
+        return  
+
+    # CASO JA EXISTA UM ARQUIVO DE MESMO NOME NA PASTA
+    nome_aux = nome_arquivo
+    i = 1
+    while os.path.exists("./dados cliente/" + nome_aux):
+        nome_aux = '(' + str(i) + ')' + nome_arquivo
+        i += 1
+
+    nome_arquivo = nome_aux
+    tamanho_aux = tamanho_arquivo
+
+    # BAIXANDO ARQUIVO
+    with open("../dados cliente/" + nome_arquivo, 'wb') as file:
+        while True:
+            if tamanho_aux <= 0:
+                break
+
+            try:
+                byte = conexao.recv(1024)
+                file.write(byte)
+                tamanho_aux -= len(byte)
+
+            except Exception as e:
+                print("Erro no download:", e)
+                return
+
+    print("Arquivo baixado!")
+
+            
+
+def cliente():
+
+    endereco = ("localhost", 12345)
+
+    socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_servidor.connect(endereco)
+
+    while True:
+
+        # MENU
+        print('\n[1] Listar arquivos\n[2] Enviar arquivo\n[3] Receber arquivo\n[4] Sair\n')
+        opcao = input("Opcao: ")
+        
+        if opcao == '1':
+            try:
+                socket_servidor.send("L".encode())
+            except Exception as e:
+                print("Erro no envio:", e)
+
+            listar_arquivos(socket_servidor)
+
+        elif opcao == '2':
+            try:
+                socket_servidor.send("A".encode())
+            except Exception as e:
+                print("Erro no envio:", e)
+
+            enviar_arquivos(socket_servidor)
+
+        elif opcao == '3':
+            try:
+                socket_servidor.send("D".encode())
+            except Exception as e:
+                print("Erro no envio:", e)
+
+            baixar_arquivos(socket_servidor)
+
+        elif opcao == '4':
+            try:
+                socket_servidor.send("sair".encode())
+            except Exception as e:
+                print("Erro no envio:", e)
+
+            socket_servidor.close()
+            break
+        else:
+            print('Opção invalida!')
+
+        
+        
+        
+        # mensagem = input("\nInforme texto ou digite 'sair' para desconectar: ")
+        # else:
+        #     arquivo = open("../dados/"+mensagem, encoding="UTF-8")
+        #     cliente_socket.send(arquivo.read().encode())
+        #     arquivo.close()
+
+if __name__ == "__main__":
+    cliente()
--- src/servidor.py
+++ src/servidor.py
@@ -1,10 +1,15 @@
-import io
-from io import FileIO
-import socket 
+import socket
+from _thread import *
+import getopt
+import sys
 import os
-import threading 
+import threading
   
-
+# try:
+#     pass
+# except Exception as e:
+#     pass
+    
 def escolha_arquivo(conexao):
 
     # ENVIANDO OPÇÕES DE DOWNLOAD
@@ -33,31 +38,108 @@ def escolha_arquivo(conexao):
         print("Erro na obtenção da opção:", e)
 
 
+def receber_arquivos():
+    pass
 
 
+def listar_arquivos(conexao):
 
-endereco = ("localhost", 12345)
-servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    arquivos = os.listdir(os.path.join(os.getcwd(), "../dados"))
+    opcoes = "Opções de download:\n"
+    i = 1
+    
+    for item in arquivos:
+        opcoes += str(i) + ". " + item + "\n"
+        i += 1 
 
-servidor_socket.bind(endereco)
-servidor_socket.listen(1)
+    try:
+        conexao.sendall(str.encode(opcoes))
+    except Exception as e:
+        print("Erro no envio:", e)
 
-while True:
-    print("\nEscutando...")
 
-    # NOVA CONEXAO
-    envio_servidor, endereco = servidor_socket.accept()
-    print("\nNova conexao recebida de ", endereco)
+def enviar_arquivos(conexao):
 
-    # ENVIANDO OPCOES DE DOWNLOAD
-    escolha_arquivo(envio_servidor)
+    # comando = "Digite o ID do arquivo escolhido (disponiveis na listagem):"
+    arquivos = os.listdir(os.path.join(os.getcwd(), "../dados"))
 
-    # RECEBENDO RESPOSTA DO CLIENTE
-    resposta = envio_servidor.recv(1024)
-    resposta = resposta.rstrip().decode()
+    # ENVIANDO COMANDO
+    # try:
+    #     conexao.sendall(comando.encode())
+    # except Exception as e:
+    #     print("Erro no envio de dados:", e)
+    #     return
 
-    if resposta != "sair":
-        print("Mensagem do cliente:\n\n\n", resposta)
-    else:
-        servidor_socket.close()
-        break
+
+    # RECEBENDO OPCAO
+    try:
+        resposta_opcao = int(conexao.recv(1024).rstrip().decode())
+        tamanho_arquivo = os.path.getsize("../dados/" + arquivos[resposta_opcao-1])
+        confirmacao = str(tamanho_arquivo) + ":" + str(arquivos[resposta_opcao-1])
+        conexao.sendall(confirmacao.encode())
+        
+    except Exception as e:
+        print("Erro no recebimento de dados:", e)
+        return
+
+
+    # ENVIANDO ARQUIVO
+    try:
+        arquivo_escolhido = open("./dados/" + str(arquivos[resposta_opcao-1]))
+        
+        while True:
+            arquivo_em_bytes = arquivo_escolhido.read(1024)
+            if len(arquivo_em_bytes) <= 0:
+                # FINALIZOU O ARQUIVO
+                break
+            conexao.sendall(arquivo_em_bytes)
+
+    except Exception as e:
+        print("Erro no envio de dados:", e)    
+
+
+def controle_cliente(conexao, endereco_cliente):
+
+    while True:
+        opcao = conexao.recv(1024).decode()
+
+        if opcao == "A":
+            receber_arquivos()
+
+        elif opcao == "L":
+            listar_arquivos(conexao)
+
+        elif opcao == "D":
+            enviar_arquivos(conexao)
+
+        elif opcao == "sair":
+            conexao.close()
+            break
+
+
+def servidor():
+
+    endereco_servidor = ("localhost", 12345)
+    servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    servidor_socket.bind(endereco_servidor)
+    servidor_socket.listen(5)
+
+    print("\nEscutando em", endereco_servidor)
+
+
+    while True:
+
+        # NOVA CONEXAO
+        conexao, endereco_cliente = servidor_socket.accept()
+        print("\nNova conexao recebida de ", endereco_cliente)
+
+        # ADICIONANDO THREAD
+        thread = threading.Thread(target = controle_cliente, args = (conexao, endereco_cliente))
+        thread.start()
+
+        print("Conexoes ativas:", threading.active_count()-1)
+
+
+if __name__ == "__main__":
+    servidor()
